ea5343d858d794c703661d8071fd2d0e3869c931
Chriswihu <chris.wihu@gmail.com>
2023-02-05 14:57:17 +0100

    setup

---
+++ src/test/java/TestCI/MainTest.java
@@ -0,0 +1,68 @@
+package TestCI;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+
+    public MainTest(){
+
+    }
+
+    @BeforeAll
+    static void beforeAll() {
+
+    }
+
+    @AfterAll
+    static void afterAll() {
+
+    }
+
+    @BeforeEach
+    void setUp() {
+        System.out.println("TESTINNNNGGGG");
+        Connection con = null;
+        try {
+            con = DBconnector.connection();
+            String createTable = "CREATE TABLE IF NOT EXISTS `startcode_test`.`usertable` (\n" +
+                    "  `id` INT NOT NULL AUTO_INCREMENT,\n" +
+                    "  `fname` VARCHAR(45) NULL,\n" +
+                    "  `lname` VARCHAR(45) NULL,\n" +
+                    "  `pw` VARCHAR(45) NULL,\n" +
+                    "  `phone` VARCHAR(45) NULL,\n" +
+                    "  `address` VARCHAR(45) NULL,\n" +
+                    "  PRIMARY KEY (`id`));";
+            con.prepareStatement(createTable).executeUpdate();
+            con.prepareStatement("DELETE FROM `startcode_test`.`usertable`").executeUpdate();
+
+            String SQL = "INSERT INTO startcode_test.usertable (fname, lname, pw, phone, address) VALUES (?,?,?,?,?)";
+            PreparedStatement ps = con.prepareStatement(SQL, Statement.RETURN_GENERATED_KEYS);
+            ps.setString(1, "Hans");
+            ps.setString(2, "Hansen");
+            ps.setString(3, "Hemmelig123");
+            ps.setString(4, "40404040");
+            ps.setString(5,"Rolighedsvej 3");
+            ps.executeUpdate();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    @AfterEach
+    void tearDown() {
+
+    }
+}
